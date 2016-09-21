ab30748a7226aac5f9f705af9a0a53be21fbf613
Talia <taliatrilling@gmail.com>
2016-09-20 18:47:50 -0700

    actually emails now

--- medreminder.py
+++ medreminder.py
@@ -1,3 +1,7 @@
+from mailin import Mailin
+import datetime
+from key import m 
+
 class Medication(object):
 	def __init__ (self, med_name, days_between_orders, days_remind_before, dose = "", email = ""):
 		self.med_name = med_name
@@ -6,22 +10,29 @@ class Medication(object):
 		self.dose = dose 
 		self.email = email
 	def send_email_reminder(self):
+		days_to_remind = int(self.days_between_orders - self.days_remind_before)
+		target = datetime.timedelta(days = days_to_remind)
+		future_target = str(datetime.datetime.now() + datetime.timedelta(days = days_to_remind))
 		if self.email == "":
 			print "You have not set an email for this reminder to go to."
-		else: #NEED SOMETHING TO MONITOR IF CORRECT # OF DAYS HAS PASSED 
-			days_to_remind = days_between_orders - days_remind_before
-			print "To: %s - Your medication, %s, needs to be renewed, as you last ordered it %s days ago" % (self.email, self.med_name, days_to_remind)
-	# def send_text_reminder(self):
-	# 	if self.mobile == "":
-	# 		print "You have not set a mobile number for this text reminder to go to."
-	# 	else: #NEED SOMETHING TO MONITOR IF CORRECT # OF DAYS HAS PASSED 
-	# 		days_to_remind = days_between_orders - days_remind_before
-	# 		print "To: %s - Your medication, %s, needs to be renewed, as you last ordered it %s days ago" % (self.mobile, self.med_name, days_to_remind)
+		else: 
+			data = { "to" : {self.email:"Reminder recipient"},
+		"from" : ["taliatrilling@gmail.com", "Talia"],
+		"subject" : "You have a medication awaiting renewal!",
+		"html" : "REMINDER: your medication, %s, needs to be renewed, as you last ordered it %s days ago. Thank you for using MedReminder!" % (self.med_name, days_to_remind),
+		"listid": [2,7],
+		"scheduled_date":future_target
+		}
+			result = m.send_email(data)
+	    	print(result)
 
-med = Medication("Placeholder med", 60, "4 mg", "taliatrilling@gmail.com")
+
+med = Medication("Placeholder med", 60, 10, email = "taliatrilling@gmail.com")
 
 def main():
 	meds_list = []
+	Medication.send_email_reminder(med)
 
 if __name__ == '__main__':
 	main() 
+
